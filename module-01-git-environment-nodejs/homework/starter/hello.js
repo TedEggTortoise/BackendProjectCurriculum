f9a9053034ad26248c3de300a1d14f17f4f6e3f2
TED <120419989+TedEggTortoise@users.noreply.github.com>
2025-10-11 10:38:47 -0700

    Homework of Hello.js

--- module-01-git-environment-nodejs/homework/starter/hello.js
+++ module-01-git-environment-nodejs/homework/starter/hello.js
@@ -2,17 +2,20 @@
 // Complete the TODOs below to create your first Node.js program
 
 // TODO: Print "Hello, Backend Development!" to the console
-
+console.log("Hello, Backend Development!");
 
 // TODO: Create a variable called 'name' with your name, then print a message like "My name is [your name]"
-
+const myName = "Ted Shi";
+console.log("My name is " + myName);
 
 // TODO: Create a variable called 'today' that gets the current date using new Date(), then print it
-
+const today = new Date();
+console.log("Today's date is: " + today);
 
 // TODO: Perform a calculation (any math operation you want) and print the result
 // Example: Calculate 5 + 3 * 2, store it in a variable, and print it
-
+const result = 5 + 3 * 2;
+console.log("Result = " + result);
 
 // When you're done:
 // 1. Run this file with: node hello.js
